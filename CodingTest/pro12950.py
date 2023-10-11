5631e27ece2af3ae530de0d3ee8ddd136ce76fb9
KIMWOOBIN <woodol4540@naver.com>
2023-10-11 23:18:03 +0900

    Day1011

--- CodingTest/pro12950.py
+++ CodingTest/pro12950.py
@@ -1,2 +1,10 @@
 def solution(arr1, arr2):
     return [[a + b for a, b in zip(sub_arr1, sub_arr2)] for sub_arr1, sub_arr2 in zip(arr1, arr2)]
+
+'''
+arr1[(5, 4), (4, 6)]
+arr2[(4, 6), (6, 2)]
+sub_arr1, sub_arr2 in zip(arr1, arr2)] 여기서 [(5, 4), (4,6)], [(4, 6), (6, 2)] 변환
+[a + b for a, b in zip(sub_arr1, sub_arr2)] 여기서 5 + 4, 4 + 6해서
+[(9, 10), (10, 8)]로 반환
+'''
